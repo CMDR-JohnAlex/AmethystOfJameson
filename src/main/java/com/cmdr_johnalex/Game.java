acd058f6f2c0bc775133cc58e08987fb9a0795d8
CMDR-JohnAlex <94056103+CMDR-JohnAlex@users.noreply.github.com>
2024-12-30 12:19:45 -0500

    Created a basic player movement function and a test.

--- src/main/java/com/cmdr_johnalex/Game.java
+++ src/main/java/com/cmdr_johnalex/Game.java
@@ -3,6 +3,14 @@ package com.cmdr_johnalex;
 import com.cmdr_johnalex.Utils.Console;
 import com.cmdr_johnalex.Utils.Input;
 
+enum Direction
+{
+    North,
+    East,
+    South,
+    West
+}
+
 // The Game class is responsible for running the game. It will init the map class, display the user interface, allow the player to move, and handle combat.
 public class Game
 {
@@ -23,6 +31,44 @@ public class Game
     {
         Map.Shutdown();
         Player.Shutdown();
+    }
+
+    // Moves the player in the given direction. Returns true if the player was able to move, false otherwise.
+    public Boolean MovePlayer(Direction direction)
+    {
+        switch (direction)
+        {
+        case North:
+            if (Player.Data.Location.second > 0)
+            {
+                Player.Data.Location.second--;
+                return true;
+            }
+            break;
+        case East:
+            if (Player.Data.Location.first < Settings.GetMapWidth() - 1)
+            {
+                Player.Data.Location.first++;
+                return true;
+            }
+            break;
+        case South:
+            if (Player.Data.Location.second < Settings.GetMapHeight() - 1)
+            {
+                Player.Data.Location.second++;
+                return true;
+            }
+            break;
+        case West:
+            if (Player.Data.Location.first > 0)
+            {
+                Player.Data.Location.first--;
+                return true;
+            }
+            break;
+        }
+
+        return false;
     }
 
     public void Run()
@@ -59,6 +105,12 @@ public class Game
         Player.Data.Gold += Map.GetRoom(Player.Data.Location).GoldAmount;
         System.out.println("You picked up " + Map.GetRoom(Player.Data.Location).GoldAmount + " gold.");
         Map.GetRoom(Player.Data.Location).Update();
+
+        System.out.println("Moving south...");
+        if (!MovePlayer(Direction.South))
+        {
+            System.out.println("You cannot move south.");
+        }
 
         // Display input options.
         System.out.println("Press enter to continue...");
